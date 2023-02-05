1d9e590091d3946f78dd8632038ddcdc545bd4ca
sdphelps <47472835+sdphelps@users.noreply.github.com>
2023-02-05 08:13:22 -0800

    Music and Sounds added
    Music and Sounds added

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayAudio : MonoBehaviour
+{
+    
+
+    //[SerializeField]
+    //private AudioSource audio;
+     
+    //private void OnTriggerExit(Collider other)
+   //{
+       //if (other.gameObject.name == "Player")
+          //audio.PlayOneShot();
+    //}
+
+}
